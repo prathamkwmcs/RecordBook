4995fd91d80e67474e200bf30b646667b2c65b9f
prathamkwmcs <pratham.kwmcs@gmail.com>
2025-03-08 23:31:24 +0530

    Update Vercel json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,5 +12,8 @@ export default defineConfig(({ mode }) => {
         "/api": env.VITE_API_URL || "http://localhost:8082", // Use loadEnv instead of import.meta.env
       },
     },
+    build: {
+      outDir: "frontend/dist", // Ensure this is set correctly
+    },
   };
 });
